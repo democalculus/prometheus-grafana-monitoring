5825c2d345f7239f70cccc92196c4e07576461c8
Eghosa <makutaworldmpm@eghosas-mbp.attlocal.net>
2024-11-17 14:36:45 -0600

    push

--- prometheus-grafana.sh
+++ prometheus-grafana.sh
@@ -5,17 +5,17 @@ sleep 60
 #Add grafana helm charts
 helm repo add grafana https://grafana.github.io/helm-charts
 sleep 60
-kubectl create ns monitor-ns
-#helm upgrade --install prometheus  prometheus/prometheus -n monitor-ns -f prometheus_values.yml
+kubectl create ns monitoring
+#helm upgrade --install prometheus  prometheus/prometheus -n monitoring -f prometheus_values.yml
 ls -lart
-helm upgrade --install prometheus  prometheus/prometheus -n monitor-ns -f prometheus_values.yml -f alertmanager_rule_11_9_24.yml
+helm upgrade --install prometheus  prometheus/prometheus -n monitoring -f prometheus_values.yml -f alertmanager_rule_11_9_24.yml
 
 sleep 60
 
 #Grafana
-helm upgrade --install  grafana grafana/grafana  -n monitor-ns
+helm upgrade --install  grafana grafana/grafana  -n monitoring
 
 sleep 60
-kubectl get secret --namespace monitor-ns grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+kubectl get secret --namespace monitoring grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
 
 kubectl apply -f montoring_rules.yml
